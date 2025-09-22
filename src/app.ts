1851c15a25caf32c06c74566a98e40c34140a8fd
SanskritiGupta <sanskriti.gupta@bounteous.com>
2025-09-22 11:29:48 +0530

    added the category module

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import { db } from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
-
+import categoriesRoutes from "./routes/categories.routes.js"
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,6 +11,7 @@ app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/categories", categoriesRoutes);
 
 app.get("/", async (_req, res) => {
   try {
