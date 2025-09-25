fd6864b85aa4f937ebfad3fda233276d625a2807
snehabounteous <sneha.balgi@bounteous.com>
2025-09-25 16:29:20 +0530

    product by category api added (#11)

--- src/models/categories.model.ts
+++ src/models/categories.model.ts
@@ -4,6 +4,7 @@ export const categories = pgTable("categories", {
   id: uuid("id").defaultRandom().primaryKey(),        // OK as integer if not referenced elsewhere
   name: text("name").notNull(),
   description: text("description"),
+  image: text("image"),
   slug: text("slug").notNull().unique(),
   created_at: timestamp("created_at").defaultNow(),
   updated_at: timestamp("updated_at").defaultNow(),
